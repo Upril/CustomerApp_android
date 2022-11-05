72fe067b1c9e75c980ed4807c5b8759263aa3126
Upril <maciej.okon@pollub.edu.pl>
2022-11-05 14:19:57 +0100

    Subscriptions code beautification, Updating Recycler on sub delete

--- app/src/main/java/com/example/untitled_project_2/adapters/SubscriptionAdapter.java
+++ app/src/main/java/com/example/untitled_project_2/adapters/SubscriptionAdapter.java
@@ -16,25 +16,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.untitled_project_2.R;
-
-import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SubscriptionAdapter extends RecyclerView.Adapter<SubscriptionAdapter.SubscriptionsAdapterViewHolder> {
-    private Activity mActivity;
-    private ArrayList<String> cities;
-    private ArrayList<String> vaccines;
-    private ArrayList<String> subIds;
-    private int length;
-    private String token;
+    private final Activity mActivity;
+    private final ArrayList<String> cities;
+    private final ArrayList<String> vaccines;
+    private final ArrayList<String> subIds;
+    private final int length;
+    private final String token;
 
     public SubscriptionAdapter(Activity activity, ArrayList<String> citiesArray, ArrayList<String> vaccinesArray, ArrayList<String> subIdsArray, int mlength, String mToken){
         mActivity = activity;
@@ -50,8 +46,7 @@ public class SubscriptionAdapter extends RecyclerView.Adapter<SubscriptionAdapte
     @Override
     public SubscriptionsAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View rowRootview = mActivity.getLayoutInflater().inflate(R.layout.sub_row,parent,false);
-        SubscriptionsAdapterViewHolder subscriptionsAdapterViewHolder = new SubscriptionsAdapterViewHolder(rowRootview);
-        return subscriptionsAdapterViewHolder;
+        return new SubscriptionsAdapterViewHolder(rowRootview);
     }
     //launches when we need to create new row
     @Override
@@ -84,25 +79,22 @@ public class SubscriptionAdapter extends RecyclerView.Adapter<SubscriptionAdapte
         public SubscriptionsAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
             //init fields, set listeners
-            SubCancelButton = (Button)itemView.findViewById(R.id.SubCancel);
-            SubCity = (TextView)itemView.findViewById(R.id.SubCity);
-            SubVaccine = (TextView)itemView.findViewById(R.id.subVaccine);
+            SubCancelButton = itemView.findViewById(R.id.SubCancel);
+            SubCity = itemView.findViewById(R.id.SubCity);
+            SubVaccine = itemView.findViewById(R.id.subVaccine);
 
-            SubCancelButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int clickedPos = (Integer)SubCancelButton.getTag();
-                    String subId = subIds.get(clickedPos);
-                    deleteSub(subId);
-                }
+            SubCancelButton.setOnClickListener(view -> {
+                int clickedPos = (Integer)SubCancelButton.getTag();
+                String subId = subIds.get(clickedPos);
+                deleteSub(subId);
             });
         }
 
         private void deleteSub(String subId) {
             RequestQueue queue = Volley.newRequestQueue(mActivity.getApplicationContext());
-            @SuppressLint("NotifyDataSetChanged") StringRequest stringRequest = new StringRequest(Request.Method.DELETE,"https://10.0.2.2:7277/api/subscriptions/"+subId+"/", response -> {
-                notifyDataSetChanged();
-            }, error -> Log.i("Error", error.toString())) {
+            @SuppressLint("NotifyDataSetChanged")
+            StringRequest stringRequest = new StringRequest(Request.Method.DELETE,"https://10.0.2.2:7277/api/subscriptions/"+subId+"/",
+                    response -> notifyDataSetChanged(), error -> Log.i("Error", error.toString())) {
                 @Override
                 public Map<String, String> getHeaders() {
                     HashMap<String, String> headers = new HashMap<>();
@@ -111,6 +103,9 @@ public class SubscriptionAdapter extends RecyclerView.Adapter<SubscriptionAdapte
                 }
             };
             queue.add(stringRequest);
+            mActivity.finish();
+            mActivity.startActivity(mActivity.getIntent());
+            Toast.makeText(mActivity.getApplicationContext(), "Usunięto subskrypcję",Toast.LENGTH_LONG).show();
         }
 
         @Override
